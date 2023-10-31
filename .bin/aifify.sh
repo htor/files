6b8d3d54586b0405ed83d8919fb840b716d96b68
Herman Torjussen <herman.torjussen@aschehoug.no>
2023-10-31 15:36:19 +0100

    add aifify

---
+++ .bin/aifify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if test $# != 1
+then
+    echo "usage: aifify [file|dir]"
+    exit 1
+fi
+
+if test -d "$1"
+then
+    find -E "$1" -not -path '*/\.*' -iregex '.*\.(flac|wav)$' -exec aifify.sh {} \;
+else
+    ffmpeg -n -hide_banner -i "$1" -write_id3v2 1 -c:v copy "${1%.*}.aiff"
+fi
+
