2bc9eee6c26a6094b0b21e89dfca0a9a45643f7e
Herman Torjussen <herman.torjussen@gmail.com>
2023-11-12 19:13:01 +0100

    aif

--- .bin/aifify.sh
+++ .bin/aifify.sh
@@ -10,7 +10,7 @@ fi
 
 if test -d "$1"
 then
-    find -E "$1" -not -path '*/\.*' -iregex '.*\.(flac|wav)$' -exec aifify.sh {} \;
+    find -E "$1" -not -path '*/\.*' -iregex '.*\.(flac|wav)$' -exec aifify.sh {} \; -exec rm -rf {} \;
 else
     ffmpeg -n -hide_banner -i "$1" -write_id3v2 1 -c:v copy "${1%.*}.aiff"
 fi
